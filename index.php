c31f609366d3ef27bc119d8e9b92c786b6faa6d7
Denimar Fernandez <fdenimar@hotmail.com>
2017-05-15 20:11:01 +0800

    First design

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <!DOCTYPE html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+		<title>ZipLogic - Unzipping Soon.</title>
+	</head>
 
+	<style type="text/css">
+		body
+		{
+			background-color: #000000;
+			color: #ffffff;
+		}
+	</style>
+	<body>
+		<div class="container">
+			<br><br><br><br><br><br><br><br><br>
+			<div>
+				<img class="img-responsive center-block" src="img/unzipping.png" style="width: 800px;">	
+				<br>
+			</div>
+			<hr>
+		</div>
+
+	</body>
+	<script type="text/javascript" src="bootstrap/dist/js/bootstrap.min.js"></script>
 </html>
